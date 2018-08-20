a029a7c9d05edbb3263aae727d0b9b715fefabfb
isurusampath1 <isurusampathwnis@gmail.com>
2018-08-20 18:41:52 +0530

    First commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+  $config = array (
+  'db_host' => 'localhost',
+  'db_name' => 'Timetable_generator',
+  'db_user' => 'root',
+  'db_pswd' => 'root',
+  'ldap_host' => '',
+  'ldap_dn' => '',
+);
+?>
